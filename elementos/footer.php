0e783e5b35e4b80c2f8efeb4e3de578acb204968
Carlos Martínez <carlos@gmail.com>
2020-06-29 22:09:17 -0600

    Agregando primer commit

---
+++ elementos/footer.php
@@ -0,0 +1,15 @@
+<div class="copyrights col-md-12" >
+	<div class="col-md-4">
+		<b>Sistema de Control de Inventario y Préstamo<br>(SICONIP)</b><br>
+	</div>
+	<div class="col-md-4">
+		<p class="text-center"> 
+			<i class="fa fa-university" aria-hidden="true"></i>&nbsp;<a href="http://curc.uls.edu.sv" target="_blank"><b>Universidad Luterana Salvadoreña</a> - 2019</b><br><b>Licencia: </b><a href="https://creativecommons.org/licenses/by/3.0/" target="_blank"><i class="fa fa-cc" aria-hidden="true"></i>&nbsp;Creative Commons</a>
+		</p>
+	</div>
+	<div class="col-md-4">
+		<p class="text-center"> 
+			<i class="fa fa-group" aria-hidden="true"></i>&nbsp;<a href="about.php"><b>Equipo de Desarrollo</b></a>
+		</p>
+	</div>
+</div>	
